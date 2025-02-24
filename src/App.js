113b21889a535c563334a30fa2a125277fd6c979
Scott Brisson <52549862+brissons@users.noreply.github.com>
2025-02-23 23:00:56 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import climb from './birthday-climb.png';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={climb} className="App-Gif" alt="Birthday Climb" />
+        </header>
+      </div>
+  );
+}
+
+export default App;
